b94e6e2ee221aaaeaf402b9205849ec572c3bb49
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-06-24 19:55:37 +0300

    Time: 23 ms (10.14%), Space: 12.3 MB (65.72%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        summa = 0
+        if root.left:
+            if not root.left.left and not root.left.right:
+                summa+=root.left.val
+            else:
+                summa+= self.sumOfLeftLeaves(root.left)
+        summa+= self.sumOfLeftLeaves(root.right)            
+        return summa
+
+        
