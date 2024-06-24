7a6f32ac578382a5a7d1c152df0f65d9ca0d811c
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-06-24 19:52:52 +0300

    Time: 16 ms (32.23%), Space: 11.5 MB (91.39%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        T =[0,1,1]
+        if n<3:
+            return T[n]
+        for i in range(3,n+1):
+            T.append(T[i-1]+T[i-2]+T[i-3])
+
+        return T[n]
