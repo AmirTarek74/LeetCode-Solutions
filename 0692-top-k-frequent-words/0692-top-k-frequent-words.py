b78ec8070b96cbd990be5b97f939bc81bd8546b5
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-07-31 12:39:30 +0300

    Time: 60 ms (43.04%), Space: 16.7 MB (56.45%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -7,17 +7,19 @@ class Solution:
         d = {}
         for word in words:
             d[word] = d.get(word,0) + 1
-        d2 = {key: value for key,value in sorted(d.items(), key=lambda item:item[1],reverse=True)}
-        keys = list(d2.keys())
-        values = list(d2.values())
+        d = {key: value for key,value in sorted(d.items(), key=lambda item:item[1],reverse=True)}
+        keys = list(d.keys())
+        values = list(d.values())
         i, j = 0, 0
         output = []
         while i != len(values) and j != len(values):
             if values[i]==values[j]:
                 j+=1
             else:
+                
                 output = output + mySort(keys[i:j])
                 i = j
+        
         output = output + mySort(keys[i:j])
         return output[0:k]
             
