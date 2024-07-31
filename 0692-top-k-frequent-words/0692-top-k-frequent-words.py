4c8cfcc4fd10c56c5d214ceb349794866327c196
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-07-31 12:37:31 +0300

    Time: 66 ms (12.14%), Space: 16.7 MB (25.18%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,24 @@
+def mySort(x):
+    x.sort()
+    return x
+    
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        d = {}
+        for word in words:
+            d[word] = d.get(word,0) + 1
+        d2 = {key: value for key,value in sorted(d.items(), key=lambda item:item[1],reverse=True)}
+        keys = list(d2.keys())
+        values = list(d2.values())
+        i, j = 0, 0
+        output = []
+        while i != len(values) and j != len(values):
+            if values[i]==values[j]:
+                j+=1
+            else:
+                output = output + mySort(keys[i:j])
+                i = j
+        output = output + mySort(keys[i:j])
+        return output[0:k]
+            
+        
