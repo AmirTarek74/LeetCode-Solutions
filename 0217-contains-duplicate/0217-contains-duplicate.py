e459ed9301c22a020b590f38d5ecc57a8657d8ca
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-08-01 14:36:49 +0300

    Time: 418 ms (54.30%), Space: 34.5 MB (14.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,7 +1,15 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set(nums)
-        if len(s)<len(nums):
-            return True
-        else:
-            return False
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d = {}
+        for num in nums:
+            if d.get(num,0)==0:
+                d[num] = d.get(num,0) + 1
+            else:
+                return True
+        
+        return False
+        
