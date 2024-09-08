0c71551f9a8c11ce3920c5819915749e88be01ac
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-09-08 11:49:32 +0300

    Time: 37 ms (65.43%), Space: 16.9 MB (22.72%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -1,35 +1,46 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-        
-        n = 0
-        ptr = head
+    def splitListToParts(
+        self, head: Optional[ListNode], k: int
+    ) -> List[Optional[ListNode]]:
         ans = [None] * k
-        while ptr:
-            n+=1
-            ptr = ptr.next
-        split = n//k
-        remainder = n%k
-        ptr = head
+
+        # get total size of linked list
+        size = 0
+        current = head
+        while current is not None:
+            size += 1
+            current = current.next
+
+        # minimum size for the k parts
+        split_size = size // k
+
+        # Remaining nodes after splitting the k parts evenly.
+        # These will be distributed to the first (size % k) nodes
+        num_remaining_parts = size % k
+
+        current = head
+        prev = current
         for i in range(k):
-            new_part = ListNode(0)
-            tail = new_part
-            
-            current_size = split
-            if remainder>0:
-                remainder -=1
-                current_size +=1
-            
-            for _ in range(current_size):
-                tail.next = ListNode(ptr.val)
-                tail = tail.next
-                ptr = ptr.next
-            
-            ans[i] = new_part.next
-        
+            # create the i-th part
+            new_part = current
+            # calculate size of i-th part
+            current_size = split_size
+            if num_remaining_parts > 0:
+                num_remaining_parts -= 1
+                current_size += 1
+
+            # traverse to end of new part
+            j = 0
+            while j < current_size:
+                prev = current
+                if current is not None:
+                    current = current.next
+                j += 1
+
+            # cut off the rest of linked list
+            if prev is not None:
+                prev.next = None
+
+            ans[i] = new_part
+
         return ans
-        
