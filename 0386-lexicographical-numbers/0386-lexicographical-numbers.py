b440cf03f10bc4cc24226fb7e8b239fc68aa6feb
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-09-22 12:19:03 +0300

    Time: 80 ms (44.55%), Space: 22.2 MB (95.69%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,10 +1,25 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        
         res = []
-        for i in range(1,n+1):
-            res.append(str(i))
         
-        res.sort()
+        for start in range(1,10):
+            self.generate_numbers(start,n,res)
         
-        return [int(c) for c in res]
+        return res
+    
+    
+    def generate_numbers(self,curr,limit,res):
+        
+        if curr>limit:
+            return
+        
+        res.append(curr)
+        
+        for next_digit in range(10):
+            next_number = curr * 10 + next_digit
+            
+            if next_number <=limit:
+                 self.generate_numbers(next_number,limit,res)
+            else:
+                break
+    
