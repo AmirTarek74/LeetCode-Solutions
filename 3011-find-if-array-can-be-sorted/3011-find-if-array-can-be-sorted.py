261b053c92603d8aeec881d9486c873e53414618
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-11-06 16:19:29 +0200

    Time: 87 ms (16.36%), Space: 16.7 MB (43.17%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+        
+        arr = nums.copy()
+        
+        for i in range(n):
+            for j in range(n-i-1):
+                if arr[j]<=arr[j+1]:
+                    continue
+                
+                else:
+                    if bin(arr[j]).count("1")==bin(arr[j+1]).count("1"):
+                        arr[j], arr[j+1] = arr[j+1], arr[j]
+                    else:
+                        return False
+        
+        return True
+        
