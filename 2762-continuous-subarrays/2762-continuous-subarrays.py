9ea352025270f605103b42e4141dc2bb36e55c2c
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-12-14 09:25:44 +0200

    Time: 830 ms (21.69%), Space: 44.5 MB (9.17%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -0,0 +1,25 @@
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        min_heap = []
+        max_heap = []
+        
+        l = 0
+        r = 0
+        res = 0
+        while r < len(nums):
+            
+            heapq.heappush(min_heap, (nums[r], r))
+            heapq.heappush(max_heap, (-nums[r], r))
+            
+            while l < r and -max_heap[0][0] - min_heap[0][0] > 2:
+                l += 1
+                while min_heap and min_heap[0][1] < l:
+                    heapq.heappop(min_heap)
+                
+                while max_heap and max_heap[0][1] < l:
+                    heapq.heappop(max_heap)
+            
+            res += r - l + 1
+            r += 1
+        
+        return res
