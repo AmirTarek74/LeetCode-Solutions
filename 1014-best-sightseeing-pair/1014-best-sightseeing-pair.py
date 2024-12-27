fec39963c1de005f31430db147b7ec05652caef7
Amir Tarek Mohamed Ali <71412332+AmirTarek74@users.noreply.github.com>
2024-12-27 11:03:52 +0200

    Time: 83 ms (21.22%), Space: 23 MB (31.71%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -2,17 +2,17 @@ class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         
         max_left = [0] * len(values)
-        max_left[0] = values[0]
+        max_left = values[0]
         
         res = 0
         
         for i in range(1, len(values)):
             curr_right = values[i] - i
             
-            res = max(res, max_left[i-1]+curr_right)
+            res = max(res, max_left+curr_right)
             
             curr_left = values[i] + i
-            max_left[i] = max(max_left[i-1], curr_left)
+            max_left = max(max_left, curr_left)
         
         
         return res
